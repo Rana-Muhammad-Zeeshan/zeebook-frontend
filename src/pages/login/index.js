9bad2c8481b70069fa63060b0c831b364377aa40
Rana Muhammad Zeeshan <ranamuhammadzeeshan92@gmail.com>
2023-01-13 16:59:28 +0500

    login validation using yup

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,35 +1,37 @@
-import { Form, Formik } from 'formik'
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import LoginInput from '../../components/inputs/logininput'
-
-import './style.css'
-
-const loginInfo = {
-  email: '',
-  password: '',
-}
-
-const Login = () => {
-  const [login, setLogin] = useState(loginInfo)
-  const { email, password } = login
-
+import "./style.css";
+import { Formik, Form } from "formik";
+import { Link } from "react-router-dom";
+import * as Yup from "yup";
+import LoginInput from "../../components/inputs/loginInput";
+import { useState } from "react";
+const loginInfos = {
+  email: "",
+  password: "",
+};
+export default function Login() {
+  const [login, setLogin] = useState(loginInfos);
+  const { email, password } = login;
   const handleLoginChange = (e) => {
-    const { name, value } = e.target
-    setLogin({ ...login, [name]: value })
-  }
-
+    const { name, value } = e.target;
+    setLogin({ ...login, [name]: value });
+  };
+  const loginValidation = Yup.object({
+    email: Yup.string()
+      .required("Email address is required.")
+      .email("Must be a valid email.")
+      .max(100),
+    password: Yup.string().required("Password is required"),
+  });
   return (
     <div className="login">
       <div className="login_wrapper">
         <div className="login_wrap">
           <div className="login_1">
-            <img src="../../icons/facebook.svg" alt="zeebook" />
+            <img src="../../icons/facebook.svg" alt="" />
             <span>
-              Zeebook helps you connect and share with the people in your life
+              Facebook helps you connect and share with the people in your life.
             </span>
           </div>
-
           <div className="login_2">
             <div className="login_2_wrap">
               <Formik
@@ -38,20 +40,22 @@ const Login = () => {
                   email,
                   password,
                 }}
+                validationSchema={loginValidation}
               >
                 {(formik) => (
                   <Form>
                     <LoginInput
-                      type="email"
+                      type="text"
                       name="email"
-                      placeholder={'Email address or phone number'}
+                      placeholder="Email address or phone number"
                       onChange={handleLoginChange}
                     />
                     <LoginInput
                       type="password"
                       name="password"
-                      placeholder={'Password'}
+                      placeholder="Password"
                       onChange={handleLoginChange}
+                      bottom
                     />
                     <button type="submit" className="blue_btn">
                       Log In
@@ -59,25 +63,20 @@ const Login = () => {
                   </Form>
                 )}
               </Formik>
-
-              <Link to={'/forgot'} className="forgot_password">
-                Forgotten password?
+              <Link to="/forgot" className="forgot_password">
+                Forgotten password ?
               </Link>
               <div className="sign_splitter"></div>
               <button className="blue_btn open_signup">Create Account</button>
             </div>
-
-            <Link to={'/'} className="sign_extra">
-              <strong>Create a page</strong>
-              &nbsp; for a celebrity, brand or business.
+            <Link to="/" className="sign_extra">
+              <b>Create a Page</b>
+              for a celebrity, brand or business.
             </Link>
           </div>
         </div>
-
         <div className="register"></div>
       </div>
     </div>
-  )
+  );
 }
-
-export default Login
